e23e68ec97d1c1c17be0b82a1dd98b7280fd42ba
sahban <sahbaan@proton.me>
2025-08-12 16:05:26 +0530

    Add Connect component for wallet connection functionality

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,9 +7,11 @@ import {
   useWaitForTransactionReceipt,
   useReadContract,
   useWriteContract,
+  useConnect,
 } from "wagmi";
 import { parseEther, formatEther } from "viem";
 import { counterAbi } from "../contracts/abi";
+import { config } from "~/components/providers/WagmiProvider";
 
 export default function Main() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
@@ -150,7 +152,7 @@ export default function Main() {
       <h1>Counter DApp</h1>
 
       {!isConnected ? (
-        <p>Please connect your wallet</p>
+       <Connect/>
       ) : (
         <>
           <h3>DEGEN COUNTER</h3>
@@ -230,6 +232,80 @@ export default function Main() {
           )}
         </>
       )}
+    </div>
+  );
+}
+
+function Connect() {
+  const { connect } = useConnect();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleConnect = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      connect({ connector: config.connectors[0] });
+    }, 500);
+
+    setTimeout(() => setIsClicked(false), 500);
+  };
+
+  return (
+    <div className="flex flex-col mt-2">
+      <button
+        onClick={handleConnect}
+        className="text-white text-center py-2 rounded-xl font-semibold text-lg shadow-lg relative overflow-hidden transform transition-all duration-200 hover:scale-110 active:scale-95 flex items-center justify-center gap-2"
+        style={{
+          background:
+            "linear-gradient(90deg, #8B5CF6, #7C3AED, #A78BFA, #8B5CF6)",
+          backgroundSize: "300% 100%",
+          animation: "gradientAnimation 3s infinite ease-in-out",
+        }}
+      >
+        <div
+          className={`absolute inset-0 bg-[#38BDF8] transition-all duration-500 ${
+            isClicked ? "scale-x-100" : "scale-x-0"
+          }`}
+          style={{ transformOrigin: "center" }}
+        ></div>
+        <style>{`
+              @keyframes gradientAnimation {
+                0% { background-position: 0% 50%; }
+                50% { background-position: 100% 50%; }
+                100% { background-position: 0% 50%; }
+              }
+            `}</style>
+        <div className="flex flex-row gap-2 px-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 relative z-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+            />
+          </svg>{" "}
+          <span className="relative z-10"> {`Connect Wallet`}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 relative z-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+            />
+          </svg>{" "}
+        </div>
+      </button>
     </div>
   );
 }
