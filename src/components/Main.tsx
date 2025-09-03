62584e5c4c86c5c836dd4ca33713e918e5dce738
sahban <sahbaan@proton.me>
2025-09-03 11:27:32 +0530

    add only accessible on farcaster client

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -204,7 +204,13 @@ export default function Main() {
       refetchLastIncrement();
       increment(context?.user.username || "Anonymous");
     }
-  }, [isConfirmed, refetchTotalCount, refetchUserCount, refetchLastIncrement, context]);
+  }, [
+    isConfirmed,
+    refetchTotalCount,
+    refetchUserCount,
+    refetchLastIncrement,
+    context,
+  ]);
 
   async function increment(username: string) {
     await fetch("/api/increment", {
@@ -228,6 +234,8 @@ export default function Main() {
       sdk.actions.addMiniApp();
     }
   }, [context?.client.added, isConfirmed]);
+
+  if ((context?.client.clientFid !== 9125)) return <Blocked />;
 
   return (
     <div className="absolute h-full w-full bg-slate-800 justify-center items-center flex flex-col">
@@ -556,6 +564,41 @@ export default function Main() {
               ))}
             </ul>
           </div>
+        </div>
+      </div>
+    );
+  }
+  function Blocked() {
+    return (
+      <div className="min-h-screen w-full bg-yellow-50 flex flex-col items-center justify-center text-yellow-800 text-center px-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+          />
+        </svg>
+
+        <h2 className="text-xl font-semibold mb-2">
+          Sorry for the inconvenience
+        </h2>
+        <p className="text-base">
+          This miniapp is designed for Fracster client only.
+        </p>
+        <div
+          className="bg-yellow-400 p-2 mt-3 text-base font-semibold cursor-pointer text-white flex items-center justify-center gap-2 rounded-lg"
+          onClick={() =>
+            sdk.actions.openUrl("https://farcaster.xyz/miniapps/jrth1IniizBC/degen-counter")
+          }
+        >
+          Open in Farcaster
         </div>
       </div>
     );
