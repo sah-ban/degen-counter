e1bc42ea9a49855990ebf2637559ce69d333c4c2
sahban <sahbaan@proton.me>
2025-09-13 15:55:57 +0530

    add functtion to switch network

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -242,6 +242,8 @@ export default function Main() {
     <div className="absolute h-full w-full bg-slate-800 justify-center items-center flex flex-col">
       {!isConnected ? (
         <Connect />
+      ) : chainId !== base.id ? (
+        <Switch />
       ) : (
         <div className="relative h-screen flex flex-col text-center shadow-2xl p-3 w-full overflow-hidden z-10">
           <header className="flex-none">
@@ -593,6 +595,78 @@ export default function Main() {
         <p className="text-base">
           This miniapp is designed for Fracster client only.
         </p>
+      </div>
+    );
+  }
+  function Switch() {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleConnect = () => {
+      setIsClicked(true);
+      setTimeout(() => {
+        switchChain({ chainId: base.id });
+      }, 500);
+
+      setTimeout(() => setIsClicked(false), 500);
+    };
+
+    return (
+      <div className="flex flex-col mt-2">
+        <button
+          onClick={handleConnect}
+          className="text-white text-center py-2 rounded-xl font-semibold text-lg shadow-lg relative overflow-hidden transform transition-all duration-200 hover:scale-110 active:scale-95 flex items-center justify-center gap-2"
+          style={{
+            background:
+              "linear-gradient(90deg, #8B5CF6, #7C3AED, #A78BFA, #8B5CF6)",
+            backgroundSize: "300% 100%",
+            animation: "gradientAnimation 3s infinite ease-in-out",
+          }}
+        >
+          <div
+            className={`absolute inset-0 bg-[#38BDF8] transition-all duration-500 ${
+              isClicked ? "scale-x-100" : "scale-x-0"
+            }`}
+            style={{ transformOrigin: "center" }}
+          ></div>
+          <style>{`
+              @keyframes gradientAnimation {
+                0% { background-position: 0% 50%; }
+                50% { background-position: 100% 50%; }
+                100% { background-position: 0% 50%; }
+              }
+            `}</style>
+          <div className="flex flex-row gap-2 px-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 relative z-10"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              />
+            </svg>{" "}
+            <span className="relative z-10">Switch to Base</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 relative z-10"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              />
+            </svg>{" "}
+          </div>
+        </button>
       </div>
     );
   }
