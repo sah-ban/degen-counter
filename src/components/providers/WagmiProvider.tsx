3103dfbc4d97dbe905b83c54ff5fa8ca836cb000
sahban <sahbaan@proton.me>
2025-09-26 10:22:56 +0530

    add walletConnect connector to WagmiProvider

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -2,13 +2,20 @@ import { createConfig, http, WagmiProvider } from "wagmi";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { walletConnect } from "wagmi/connectors";
+
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "";
+
 
 export const config = createConfig({
   chains: [base],
   transports: {
     [base.id]: http(),
   },
-  connectors: [farcasterFrame()],
+  connectors: [farcasterFrame(), walletConnect({
+      projectId,
+      showQrModal: true,
+    })],
 });
 
 const queryClient = new QueryClient();
