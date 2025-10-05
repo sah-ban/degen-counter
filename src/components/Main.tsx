0638b2fae900f881ef568c6ff37358ebb51f3010
sahban <sahbaan@proton.me>
2025-10-05 14:12:46 +0530

    remove leaderboard

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,15 +16,11 @@ import { counterAbi } from "../contracts/abi";
 import { config } from "~/components/providers/WagmiProvider";
 import { base } from "wagmi/chains";
 import { formatUnits } from "viem";
-import Leaderboard from "./Leaderboard";
 import { blocked } from "./blocked";
 
 export default function Main() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
   const [context, setContext] = useState<Context.MiniAppContext>();
-  const [activeDiv, setActiveDiv] = useState<"DEGEN Counter" | "Leaderboard">(
-    "DEGEN Counter"
-  );
 
   useEffect(() => {
     const load = async () => {
@@ -62,6 +58,7 @@ export default function Main() {
   const { isSuccess: isApproved } = useWaitForTransactionReceipt({
     hash: approveHash,
   });
+  const [isClicked, setIsClicked] = useState(false);
 
   const formatTimeElapsed = (timestamp: string | number | undefined) => {
     if (!timestamp || timestamp === "never") return "Never";
@@ -202,7 +199,6 @@ export default function Main() {
       refetchTotalCount();
       refetchUserCount();
       refetchLastIncrement();
-      increment(context?.user.username || "Anonymous");
     }
   }, [
     isConfirmed,
@@ -212,19 +208,6 @@ export default function Main() {
     context,
   ]);
 
-  useEffect(() => {
-    if (isConfirmed && context?.user.fid !== 268438) {
-      increment(context?.user.username || "Anonymous");
-    }
-  }, [isConfirmed, context]);
-
-  async function increment(username: string) {
-    await fetch("/api/increment", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username }),
-    });
-  }
 
   useEffect(() => {
     if (!context?.client.added && isConfirmed) {
@@ -245,7 +228,31 @@ export default function Main() {
       }),
     });
   }
+  const cast = async (): Promise<string | undefined> => {
+    try {
+      const result = await sdk.actions.composeCast({
+        text: `Just incremented the DEGEN counter to ${totalCount}!`,
+        embeds: [`${process.env.NEXT_PUBLIC_URL}?count=${totalCount}`],
+      });
+      return result.cast?.hash;
+    } catch (error) {
+      console.error("Error composing cast:", error);
+      return undefined;
+    }
+  };
   const balance = Number(contractBalance);
+  const inc = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      if (isConfirmed) {
+        cast();
+      } else {
+        handleIncrement();
+      }
+    }, 500);
+
+    setTimeout(() => setIsClicked(false), 500);
+  };
 
   useEffect(() => {
     if (context && rawContractBalance && balance < 69) {
@@ -294,41 +301,168 @@ export default function Main() {
           <header className="flex-none">
             <div className="flex items-center">
               <div className="flex-grow" />
-              <div className="text-3xl font-bold text-sky-400">{activeDiv}</div>
-
+              <div className="text-3xl font-bold text-sky-400">
+                DEGEN Counter
+              </div>
               <div className="flex-grow" />
             </div>
           </header>
 
-          <main className="flex-1 overflow-auto">
-            {activeDiv === "DEGEN Counter" && <Counter />}
-            {activeDiv === "Leaderboard" && <Leaderboard />}
-          </main>
-
-          <footer className="flex-none text-center rounded-xl shadow p-2 bg-slate-900">
-            <div className="flex justify-around font-bold">
-              <button
-                className={`p-2 ${
-                  activeDiv === "DEGEN Counter"
-                    ? "bg-[#A36EFD] text-black"
-                    : "bg-gray-600 text-white"
-                } rounded`}
-                onClick={() => setActiveDiv("DEGEN Counter")}
-              >
-                Counter
-              </button>
-              <button
-                className={`p-2 ${
-                  activeDiv === "Leaderboard"
-                    ? "bg-[#A36EFD] text-black"
-                    : "bg-gray-600 text-white"
-                } rounded`}
-                onClick={() => setActiveDiv("Leaderboard")}
-              >
-                Leaderboard
-              </button>
+          <div className="relative flex flex-col items-center justify-center h-full w-full overflow-hidden">
+            <div className="flex justify-around w-full mt-4 z-10">
+              <div className="flex justify-center w-full mt-4 space-x-6 z-10">
+                <div className="bg-white bg-opacity-20 px-4 py-2 rounded-lg text-center">
+                  <p className="text-gray-300 text-sm">You incremented</p>
+                  <span className="text-lg font-bold text-white">
+                    {userCount !== undefined
+                      ? userCount.toString()
+                      : "Loading..."}
+                  </span>
+                </div>
+                <div className="bg-white bg-opacity-20 px-4 py-2 rounded-lg text-center">
+                  <p className="text-gray-300 text-sm">Last Increment</p>
+                  <span className="text-lg font-bold text-white">
+                    {formatTimeElapsed(lastIncrement) ?? "Loading..."}
+                  </span>
+                </div>
+              </div>
             </div>
-          </footer>
+
+            <div className="relative flex flex-col items-center text-white mt-6">
+              <div className="relative flex flex-col items-center">
+                <div className="text-lg font-bold text-center">
+                  Total incremented
+                </div>
+              </div>
+              <div>
+                <div className="text-center text-7xl font-extrabold text-lime-500">
+                  {totalCount ?? "Loading..."}
+                </div>
+              </div>
+
+              <div className="flex flex-col mt-2">
+                <button
+                  onClick={inc}
+                  className="text-white text-center py-2 rounded-xl font-semibold text-lg shadow-lg relative overflow-hidden transform transition-all duration-200 hover:scale-110 active:scale-95 flex items-center justify-center gap-2"
+                  style={{
+                    background:
+                      "linear-gradient(90deg, #8B5CF6, #7C3AED, #A78BFA, #8B5CF6)",
+                    backgroundSize: "300% 100%",
+                    animation: "gradientAnimation 3s infinite ease-in-out",
+                  }}
+                >
+                  <div
+                    className={`absolute inset-0 bg-[#38BDF8] transition-all duration-500 ${
+                      isClicked ? "scale-x-100" : "scale-x-0"
+                    }`}
+                    style={{ transformOrigin: "center" }}
+                  ></div>
+                  <style>{`
+              @keyframes gradientAnimation {
+                0% { background-position: 0% 50%; }
+                50% { background-position: 100% 50%; }
+                100% { background-position: 0% 50%; }
+              }
+            `}</style>
+                  <div className="flex flex-row gap-2 px-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 relative z-10"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                      />
+                    </svg>{" "}
+                    <span className="relative z-10">
+                      {" "}
+                      {isPending
+                        ? "Processing..."
+                        : isConfirming
+                        ? "Incrementing..."
+                        : isConfirmed
+                        ? "Incremented!, Cast it!"
+                        : "Increment"}
+                    </span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 relative z-10"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                      />
+                    </svg>{" "}
+                  </div>
+                </button>
+              </div>
+            </div>
+            <button
+              onClick={() =>
+                sdk.actions.openMiniApp({
+                  url: "https://quotes-miniapp.vercel.app",
+                })
+              }
+              className="bg-[#7C3AED] text-white px-4 py-2 rounded-lg hover:bg-[#38BDF8] transition cursor-pointer font-semibold mt-4 w-2/3"
+            >
+              Claim $ARB
+            </button>
+            {context?.user.fid === 268438 && (
+              <div className="relative text-center mt-5 backdrop-blur rounded-xl shadow-2xl p-6 w-full max-w-md border border-gray-700 z-10">
+                <div className="text-gray-300 font-medium flex flex-row space-x-2 justify-center">
+                  <div>
+                    <span className="font-bold text-purple-400">Balance:</span>{" "}
+                    {contractBalance !== undefined
+                      ? contractBalance
+                      : "Loading..."}
+                  </div>
+                  <div>
+                    <span className="font-bold text-purple-400">Amount:</span>{" "}
+                    {tokenAmount !== undefined ? tokenAmount : "Loading..."}
+                  </div>
+                </div>
+
+                <div className="flex flex-col">
+                  <div className="flex items-center">
+                    <input
+                      type="number"
+                      value={depositAmount}
+                      onChange={(e) => setDepositAmount(e.target.value)}
+                      className="w-full px-3 py-2 bg-gray-400 border border-gray-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500/30"
+                    />
+                    <button
+                      onClick={handleDepositTokens}
+                      disabled={isPending || isConfirming}
+                      className="px-4 py-2 rounded-lg bg-gradient-to-r from-indigo-700 via-purple-600 to-fuchsia-600 text-white font-medium shadow-lg ring-2 ring-purple-500/30 disabled:opacity-50 disabled:cursor-not-allowed"
+                    >
+                      Deposit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {writeError && (
+              <p className="relative text-red-400 text-center text-xs font-medium z-10 hidden">
+                Error: {writeError.message}
+              </p>
+            )}
+            {isConfirmed && (
+              <p className="relative text-lime-500 text-center text-base font-medium mt-4">
+                Come after 6 hours to increment again! <br />
+              </p>
+            )}
+          </div>
         </div>
       )}
     </div>
@@ -410,190 +544,6 @@ export default function Main() {
             </svg>{" "}
           </div>
         </button>
-      </div>
-    );
-  }
-
-  function Counter() {
-    const [isClicked, setIsClicked] = useState(false);
-
-    const cast = async (): Promise<string | undefined> => {
-      try {
-        const result = await sdk.actions.composeCast({
-          text: `Just incremented the DEGEN counter to ${totalCount}!`,
-          embeds: [`${process.env.NEXT_PUBLIC_URL}?count=${totalCount}`],
-        });
-        return result.cast?.hash;
-      } catch (error) {
-        console.error("Error composing cast:", error);
-        return undefined;
-      }
-    };
-
-    const inc = () => {
-      setIsClicked(true);
-      setTimeout(() => {
-        if (isConfirmed) {
-          cast();
-        } else {
-          handleIncrement();
-        }
-      }, 500);
-
-      setTimeout(() => setIsClicked(false), 500);
-    };
-
-    return (
-      <div className="relative flex flex-col items-center justify-center h-full w-full overflow-hidden">
-        {/* Your Count and Last Increment Side by Side */}
-        <div className="flex justify-around w-full mt-4 z-10">
-          <div className="flex justify-center w-full mt-4 space-x-6 z-10">
-            <div className="bg-white bg-opacity-20 px-4 py-2 rounded-lg text-center">
-              <p className="text-gray-300 text-sm">You incremented</p>
-              <span className="text-lg font-bold text-white">
-                {userCount !== undefined ? userCount.toString() : "Loading..."}
-              </span>
-            </div>
-            <div className="bg-white bg-opacity-20 px-4 py-2 rounded-lg text-center">
-              <p className="text-gray-300 text-sm">Last Increment</p>
-              <span className="text-lg font-bold text-white">
-                {formatTimeElapsed(lastIncrement) ?? "Loading..."}
-              </span>
-            </div>
-          </div>
-        </div>
-
-        {/* Image with Total Count Overlay */}
-        <div className="relative flex flex-col items-center text-white mt-6">
-          <div className="relative flex flex-col items-center">
-            <div className="text-lg font-bold text-center">
-              Total incremented
-            </div>
-          </div>
-          <div>
-            <div className="text-center text-7xl font-extrabold text-lime-500">
-              {totalCount ?? "Loading..."}
-            </div>
-          </div>
-
-          <div className="flex flex-col mt-2">
-            <button
-              onClick={inc}
-              className="text-white text-center py-2 rounded-xl font-semibold text-lg shadow-lg relative overflow-hidden transform transition-all duration-200 hover:scale-110 active:scale-95 flex items-center justify-center gap-2"
-              style={{
-                background:
-                  "linear-gradient(90deg, #8B5CF6, #7C3AED, #A78BFA, #8B5CF6)",
-                backgroundSize: "300% 100%",
-                animation: "gradientAnimation 3s infinite ease-in-out",
-              }}
-            >
-              <div
-                className={`absolute inset-0 bg-[#38BDF8] transition-all duration-500 ${
-                  isClicked ? "scale-x-100" : "scale-x-0"
-                }`}
-                style={{ transformOrigin: "center" }}
-              ></div>
-              <style>{`
-              @keyframes gradientAnimation {
-                0% { background-position: 0% 50%; }
-                50% { background-position: 100% 50%; }
-                100% { background-position: 0% 50%; }
-              }
-            `}</style>
-              <div className="flex flex-row gap-2 px-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 relative z-10"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-                  />
-                </svg>{" "}
-                <span className="relative z-10">
-                  {" "}
-                  {isPending
-                    ? "Processing..."
-                    : isConfirming
-                    ? "Incrementing..."
-                    : isConfirmed
-                    ? "Incremented!, Cast it!"
-                    : "Increment"}
-                </span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 relative z-10"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-                  />
-                </svg>{" "}
-              </div>
-            </button>
-          </div>
-        </div>
-            <button
-              onClick={() =>
-                sdk.actions.openMiniApp({url: "https://quotes-miniapp.vercel.app"})
-              }
-              className="bg-[#7C3AED] text-white px-4 py-2 rounded-lg hover:bg-[#38BDF8] transition cursor-pointer font-semibold mt-4 w-2/3"
-            >
-              Claim $ARB
-            </button>
-        {context?.user.fid === 268438 && (
-          <div className="relative text-center mt-5 backdrop-blur rounded-xl shadow-2xl p-6 w-full max-w-md border border-gray-700 z-10">
-            <div className="text-gray-300 font-medium flex flex-row space-x-2 justify-center">
-              <div>
-                <span className="font-bold text-purple-400">Balance:</span>{" "}
-                {contractBalance !== undefined ? contractBalance : "Loading..."}
-              </div>
-              <div>
-                <span className="font-bold text-purple-400">Amount:</span>{" "}
-                {tokenAmount !== undefined ? tokenAmount : "Loading..."}
-              </div>
-            </div>
-
-            <div className="flex flex-col">
-              <div className="flex items-center">
-                <input
-                  type="number"
-                  value={depositAmount}
-                  onChange={(e) => setDepositAmount(e.target.value)}
-                  className="w-full px-3 py-2 bg-gray-400 border border-gray-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500/30"
-                />
-                <button
-                  onClick={handleDepositTokens}
-                  disabled={isPending || isConfirming}
-                  className="px-4 py-2 rounded-lg bg-gradient-to-r from-indigo-700 via-purple-600 to-fuchsia-600 text-white font-medium shadow-lg ring-2 ring-purple-500/30 disabled:opacity-50 disabled:cursor-not-allowed"
-                >
-                  Deposit
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-
-        {writeError && (
-          <p className="relative text-red-400 text-center text-xs font-medium z-10 hidden">
-            Error: {writeError.message}
-          </p>
-        )}
-        {isConfirmed && (
-          <p className="relative text-lime-500 text-center text-base font-medium mt-4">
-            Come after 6 hours to increment again! <br />
-          </p>
-        )}
       </div>
     );
   }
