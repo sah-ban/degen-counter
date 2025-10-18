ccfee32c92fc850d571356c556e8217168ceb7c3
sahban <sahbaan@proton.me>
2025-10-18 23:03:02 +0530

    change open miniapp

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -390,12 +390,12 @@ export default function Main() {
             <button
               onClick={() =>
                 sdk.actions.openMiniApp({
-                  url: "https://quotes-miniapp.vercel.app",
+                  url: "https://gif.itscashless.com",
                 })
               }
               className="bg-[#7C3AED] text-white px-4 py-2 rounded-lg hover:bg-[#38BDF8] transition cursor-pointer font-semibold mt-4 w-2/3"
             >
-              Claim $ARB
+              Claim $USDC
             </button>
             {context?.user.fid === 268438 && (
               <div className="relative text-center mt-5 backdrop-blur rounded-xl shadow-2xl p-6 w-full max-w-md border border-gray-700 z-10">
