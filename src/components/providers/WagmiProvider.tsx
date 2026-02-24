e0ae09c5591af8cf1d2ad59ed95df57711f694f4
sahban <sahbaan@proton.me>
2026-02-24 12:31:19 +0530

    add data suffix with builder code

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -2,13 +2,18 @@ import { createConfig, http, WagmiProvider } from "wagmi";
 import { base } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
+import { Attribution } from "ox/erc8021";
 
+const DATA_SUFFIX = Attribution.toDataSuffix({
+  codes: ["bc_llyxg7yz"],
+});
 
 export const config = createConfig({
   chains: [base],
   transports: {
     [base.id]: http(),
   },
+  dataSuffix: DATA_SUFFIX,
   connectors: [farcasterMiniApp()],
 });
 
